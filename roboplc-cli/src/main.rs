45c2e58f4c575927800ced545f16e5764c293d1d
Serhij S <div@altertech.com>
2024-04-07 23:21:22 +0200

    url trim slashes

--- roboplc-cli/src/main.rs
+++ roboplc-cli/src/main.rs
@@ -362,7 +362,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         robo_toml.replace(value);
     }
     let timeout = timeout.unwrap_or(DEFAULT_TIMEOUT);
-    let url = url.ok_or("URL not specified")?;
+    let url_s = url.ok_or("URL not specified")?;
+    let url = url_s.trim_end_matches('/');
     let key = key.ok_or("Key not specified")?;
     let agent: Agent = ureq::AgentBuilder::new()
         .timeout_read(Duration::from_secs(timeout))
@@ -370,19 +371,19 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .build();
     match args.subcmd {
         SubCommand::Stat => {
-            stat_command(&url, &key, agent)?;
+            stat_command(url, &key, agent)?;
         }
         SubCommand::Config => {
-            set_mode_command(&url, &key, agent, Mode::Config)?;
+            set_mode_command(url, &key, agent, Mode::Config)?;
         }
         SubCommand::Run => {
-            set_mode_command(&url, &key, agent, Mode::Run)?;
+            set_mode_command(url, &key, agent, Mode::Run)?;
         }
         SubCommand::Flash(opts) => {
-            flash(&url, &key, agent, opts, robo_toml)?;
+            flash(url, &key, agent, opts, robo_toml)?;
         }
         SubCommand::Purge => {
-            purge_command(&url, &key, agent)?;
+            purge_command(url, &key, agent)?;
         }
     }
     Ok(())
